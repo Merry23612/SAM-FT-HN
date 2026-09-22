8ff6f80d1dadbc031c4a5630b8b0e6615ecf825d
Merry23612 <merry.xia@hotmail.com>
2026-09-22 18:04:35 -0500

    Clean up postprocessing.py by removing comments
    Removed metadata comments and docstring from the resample_nii function.

--- utils/postprocessing.py
+++ utils/postprocessing.py
@@ -1,12 +1,3 @@
-# -*- encoding: utf-8 -*-
-'''
-@File    :   prepare_data_from_nnUNet.py
-@Time    :   2023/12/10 23:07:39
-@Author  :   Haoyu Wang 
-@Contact :   small_dark@sina.com
-@Brief   :   pre-process nnUNet-style dataset into SAM-Med3D-style
-'''
-
 import os.path as osp
 import os
 import json
@@ -16,16 +7,9 @@ from tqdm import tqdm
 import torchio as tio
 
 def resample_nii(input_path: str, output_path: str, target_spacing: tuple = (1.5, 1.5, 1.5), n=None, reference_image=None, mode="linear"):
-    """
-    Resample a nii.gz file to a specified spacing using torchio.
-
-    Parameters:
-    - input_path: Path to the input .nii.gz file.
-    - output_path: Path to save the resampled .nii.gz file.
-    - target_spacing: Desired spacing for resampling. Default is (1.5, 1.5, 1.5).
-    """
+  
     
-    # Load the nii.gz file using torchio
+    
     subject = tio.Subject(
         img=tio.ScalarImage(input_path)
     )
